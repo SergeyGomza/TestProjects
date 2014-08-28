0ca2fa78c13199b3701014a5f3339f6200fdd620
Sergey <s.gomza@itransition.com>
2014-08-28 18:35:49 +0300

    Testing news loader project.

---
+++ NewsDownloader/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace NewsDownloader.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public String GetNews()
+        {
+            StreamReader sr = new StreamReader(@"D:\Projects\News\NewsDownloader\NewsDownloader\JSON\news-headlines.json");
+            var result = sr.ReadToEnd();
+            return result;
+        }
+        public ActionResult MySpace()
+        {
+            return View();
+        }
+    }
+}
